89530103c09ed7ee3951e2a256004c77bc6c6313
Yury Hayeu <ggaev.work@gmail.com>
2024-03-04 15:07:34 +0100

    Allow to specify policy estimation method for policy

--- diploma_thesis/agents/utils/policy/discrete_action.py
+++ diploma_thesis/agents/utils/policy/discrete_action.py
@@ -5,48 +5,69 @@ from agents.utils import NeuralNetwork, Phase
 from agents.utils.nn.layers.linear import Linear
 from agents.utils.action import ActionSelector, from_cli as action_selector_from_cli
 from .policy import *
+from enum import StrEnum
+
+
+class PolicyEstimationMethod(StrEnum):
+    DUELING_ARCHITECTURE = "dueling_architecture"
+    INDEPENDENT = "independent"
 
 
 class DiscreteAction(Policy[Input]):
 
     def __init__(self,
                  n_actions: int,
-                 q_model: NeuralNetwork,
-                 advantage_model: NeuralNetwork | None,
-                 action_selector: ActionSelector):
+                 value_model: NeuralNetwork,
+                 action_model: NeuralNetwork,
+                 action_selector: ActionSelector,
+                 value_estimation_method: PolicyEstimationMethod = PolicyEstimationMethod.INDEPENDENT):
         super().__init__()
 
         self.n_actions = n_actions
-        self.q_model = q_model
-        self.advantage_model = advantage_model
+        self.value_model = value_model
+        self.action_model = action_model
         self.action_selector = action_selector
+        self.policy_estimation_method = value_estimation_method
 
         self.__configure__()
 
     def update(self, phase: Phase):
         self.phase = phase
 
-        for module in [self.q_model, self.advantage_model, self.action_selector]:
+        for module in [self.value_model, self.advantage_model, self.action_selector]:
             if isinstance(module, PhaseUpdatable):
                 module.update(phase)
 
     def __call__(self, state: State, parameters: Input) -> Record:
-        values = self.predict(state).view(-1)
-        action, policy = self.action_selector(values)
+        values, actions = self.predict(state).view(-1)
+        action, policy = self.action_selector(actions)
         action = action if torch.is_tensor(action) else torch.tensor(action, dtype=torch.long)
 
-        info = TensorDict({"policy": policy, "values": values.detach().clone()}, batch_size=[])
+        info = TensorDict({
+            "policy": policy,
+            "values": values.detach().clone(),
+            "actions": actions.detach().clone()
+        }, batch_size=[])
 
         return Record(state, action, info, batch_size=[])
 
-    def predict(self, state: State) -> torch.FloatTensor:
-        values = self.q_model(state)
+    def predict(self, state: State):
+        values = None
+        actions = None
+
+        if self.value_model is not None:
+            values = self.value_model(state)
 
         if self.advantage_model is not None:
-            advantages = self.advantage_model(state)
-            values = values + advantages - advantages.mean(dim=1, keepdim=True)
+            actions = self.action_model(state)
 
-        return values
+        match self.policy_estimation_method:
+            case PolicyEstimationMethod.INDEPENDENT:
+                return values, actions
+            case PolicyEstimationMethod.DUELING_ARCHITECTURE:
+                return values, values + (actions - actions.mean(dim=1, keepdim=True))
+            case _:
+                raise ValueError(f"Policy estimation method {self.policy_estimation_method} is not supported")
 
     def clone(self):
         return copy.deepcopy(self)
@@ -54,20 +75,17 @@ class DiscreteAction(Policy[Input]):
     # Utilities
 
     def __configure__(self):
-        value_output_layer = Linear(dim=1, activation='none', dropout=0)
-        action_output_layer = Linear(dim=self.n_actions, activation='none', dropout=0)
+        if self.action_model is not None:
+            self.action_model.append_output_layer(Linear(dim=self.n_actions, activation='none', dropout=0))
 
-        if self.advantage_model is not None:
-            self.q_model.append_output_layer(value_output_layer)
-            self.advantage_model.append_output_layer(action_output_layer)
-        else:
-            self.q_model.append_output_layer(action_output_layer)
+        if self.value_model is not None:
+            self.value_model.append_output_layer(Linear(dim=1, activation='none', dropout=0))
 
     @staticmethod
     def from_cli(parameters: Dict) -> 'Policy':
         n_actions = parameters['n_actions']
-        q_model = NeuralNetwork.from_cli(parameters['q_model'])
-        advantage_model = NeuralNetwork.from_cli(parameters['advantage_model']) if parameters.get('advantage_model') else None
+        value_model = NeuralNetwork.from_cli(parameters['value_model']) if parameters.get('value_model') else None
+        action_model = NeuralNetwork.from_cli(parameters['action_model']) if parameters.get('action_model') else None
         action_selector = action_selector_from_cli(parameters['action_selector'])
 
-        return DiscreteAction(n_actions, q_model, advantage_model, action_selector)
+        return DiscreteAction(n_actions, value_model, action_model, action_selector)
