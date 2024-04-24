5c20494343203510806393f049c34ef5c0a9908a
Yury Hayeu <ggaev.work@gmail.com>
2024-04-24 13:11:07 +0200

    Change training schedule

--- diploma_thesis/agents/utils/rl/dqn.py
+++ diploma_thesis/agents/utils/rl/dqn.py
@@ -1,12 +1,11 @@
-from dataclasses import dataclass
 from typing import Dict
 
 import tensordict
 from torch.optim.swa_utils import AveragedModel, get_ema_avg_fn
 
 from agents.utils.memory import NotReadyException
-from agents.utils.rl.rl import *
 from agents.utils.return_estimator import ValueFetchMethod
+from agents.utils.rl.rl import *
 
 
 class DeepQTrainer(RLTrainer):
@@ -14,12 +13,14 @@ class DeepQTrainer(RLTrainer):
     class Configuration:
         decay: float = 0.99
         update_steps: int = 10
+        epochs: int = 1
 
         @staticmethod
         def from_cli(parameters: Dict):
             return DeepQTrainer.Configuration(
                 decay=parameters.get('decay', 0.99),
-                update_steps=parameters.get('update_steps', 20)
+                update_steps=parameters.get('update_steps', 20),
+                epochs=parameters.get('epochs', 1)
             )
 
     def __init__(self, configuration: Configuration, *args, **kwargs):
@@ -37,34 +38,35 @@ class DeepQTrainer(RLTrainer):
         self._target_model = AveragedModel(model.clone(), avg_fn=avg_fn).to(self.device)
 
     def __train__(self, model: Policy):
-        try:
-            batch, info = self.storage.sample(device=self.device)
-        except NotReadyException:
-            return
+        for _ in range(self.configuration.epochs):
+            try:
+                batch, info = self.storage.sample(device=self.device)
+            except NotReadyException:
+                return
 
-        with torch.no_grad():
-            q_values = self.estimate_q(model, batch)
+            with torch.no_grad():
+                q_values = self.estimate_q(model, batch)
 
-        def compute_loss():
-            actions = self.__get_action_values__(model, batch.state, batch.action)
+            def compute_loss():
+                actions = self.__get_action_values__(model, batch.state, batch.action)
 
-            loss_ = self.loss(actions, q_values)
-            td_error_ = torch.square(actions - q_values)
+                loss_ = self.loss(actions, q_values)
+                td_error_ = torch.square(actions - q_values)
 
-            entropy = torch.distributions.Categorical(logits=actions).entropy().mean()
+                entropy = torch.distributions.Categorical(logits=actions).entropy().mean()
 
-            return loss_, (td_error_, entropy)
+                return loss_, (td_error_, entropy)
 
-        loss, result = self.step(compute_loss, self.optimizer)
+            loss, result = self.step(compute_loss, self.optimizer)
 
-        td_error, entropy = result
-        td_error_mean = td_error.mean()
+            td_error, entropy = result
+            td_error_mean = td_error.mean()
 
-        self.record_loss(loss)
-        self.record_loss(td_error_mean, key='td_error')
-        self.record_loss(entropy, key='entropy')
+            self.record_loss(loss)
+            self.record_loss(td_error_mean, key='td_error')
+            self.record_loss(entropy, key='entropy')
 
-        print(f'loss: {loss}, td_error: {td_error_mean}, entropy: {entropy}')
+            print(f'loss: {loss}, td_error: {td_error_mean}, entropy: {entropy}')
 
         with torch.no_grad():
             if self.optimizer.step_count % self.configuration.update_steps == 0:
