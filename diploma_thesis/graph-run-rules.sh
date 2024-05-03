2a39b045b6be2854f5161ff70c49cc7b62959a3b
Yury Hayeu <ggaev.work@gmail.com>
2024-05-03 23:13:01 +0200

    Fix cuda for ppo

--- diploma_thesis/graph-run-rules.sh
+++ diploma_thesis/graph-run-rules.sh
@@ -1,11 +1,15 @@
 
 
-python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/3/0/experiment.yml'
+python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/3/0/experiment.yml' &
 
-python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/3/1/experiment.yml'
-#
-python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/3/2/experiment.yml'
-#
-python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/3/3/experiment.yml'
+python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/3/1/experiment.yml' &
 
-python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/3/4/experiment.yml'
+wait
+#
+python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/3/2/experiment.yml' &
+#
+python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/3/3/experiment.yml' &
+
+python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/3/4/experiment.yml' &
+
+wait
--- diploma_thesis/graph-run.sh
+++ diploma_thesis/graph-run.sh
@@ -1,11 +1,15 @@
 
 
-python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/2/0/experiment.yml'
+python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/2/0/experiment.yml' &
 
-python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/2/1/experiment.yml'
-#
-python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/2/2/experiment.yml'
-#
-python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/2/3/experiment.yml'
+python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/2/1/experiment.yml' &
 
-python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/2/4/experiment.yml'
+wait
+#
+python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/2/2/experiment.yml' &
+#
+python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/2/3/experiment.yml' &
+
+python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/2/4/experiment.yml' &
+
+wait
