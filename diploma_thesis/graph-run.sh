09ce0b89fd83064ed5d3fc516ec14730cdd014ba
Yury Hayeu <ggaev.work@gmail.com>
2024-05-04 09:47:11 +0200

    Fixes

--- diploma_thesis/graph-run.sh
+++ diploma_thesis/graph-run.sh
@@ -1,13 +1,11 @@
 
 
+python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/1/0/experiment.yml' &
+
 python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/2/0/experiment.yml' &
 
-python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/2/1/experiment.yml'
-#
-python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/2/2/experiment.yml'
-#
-python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/2/3/experiment.yml'
+python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/3/0/experiment.yml' &
 
-python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/2/4/experiment.yml'
+python cli.py --configuration 'configuration/experiments/jsp/GRAPH-NN/experiments/4/0/experiment.yml' &
 
 wait
